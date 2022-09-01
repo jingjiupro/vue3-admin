bbbd7a345690e8c3a05eae68c83f5dded1abff83
yilujian <1520406381@qq.com>
2022-09-01 10:08:21 +0800

    测试husky

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-	const count = 0
+const count = 0
+
 	console.log(count)
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
